0064bcb5d7aba1ae55d2156af8dcb720023e4ab1
rushabh <rushabhgediya38@gmail.com>
2021-05-18 15:17:10 +0530

    hosts edit

--- WFcounter/settings.py
+++ WFcounter/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-=w!_9s(ahr9$iqv%!+@z-s&z7bnzwz(brkii3==pxmyk2z9xoc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['website-fetching.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
